99eaa3c8bdbc626a4a324e741e8d40bf194b9c8b
NzambiKai <mail@troyrizzo.com>
2024-09-19 13:51:31 +1000

    Code updates

--- script.js
+++ script.js
@@ -71,9 +71,11 @@ const generateBtn = document.getElementById('generate-btn');
 const raidDetails = document.getElementById('raid-details');
 const raidImage = document.getElementById('raid-image');
 const raidName = document.getElementById('raid-name');
-const raidLocation = document.getElementById('raid-location');
-const raidRequires = document.getElementById('raid-requires');
-const extraChallenge = document.getElementById('extra-challenge');
+const locationName = document.getElementById('location-name');
+const requiresImage = document.getElementById('requires-image');
+const requiresName = document.getElementById('requires-name');
+const challengeImage = document.getElementById('challenge-image');
+const challengeName = document.getElementById('challenge-name');
 
 generateBtn.addEventListener('click', () => {
     const randomRaid = raidActivities[Math.floor(Math.random() * raidActivities.length)];
@@ -81,9 +83,10 @@ generateBtn.addEventListener('click', () => {
     const randomSubclass = subclasses[Math.floor(Math.random() * subclasses.length)];
     raidImage.src = `images/raids/${randomRaid.imagePath}`;
     raidName.textContent = randomRaid.activity;
-    raidLocation.innerHTML = `<h4>Location</h4><p>${randomRaid.location}</p>`;
-    raidRequires.innerHTML = `<h4>Requires</h4><img id="requires-image" src="images/icons/${randomRaid.requiresImagePath}" alt="Requires Image"><p>${randomRaid.requires}</p>`;
-    extraChallenge.innerHTML = `<h4>Extra Challenge</h4><p>Run all ${randomClass} and only ${randomSubclass} subclass.</p>`;
+    locationName.textContent = ${randomRaid.location};
+    requiresImage.src = `images/icons/${randomRaid.requiresImagePath}`;
+    requiresName.textContent = ${randomRaid.requires};
+    //`<h4>Extra Challenge</h4><p>Run all ${randomClass} and only ${randomSubclass} subclass.</p>`;
     raidDetails.style.display = 'block';
 });
 
